408c8ca8d0e700ffb084a4d952e816d215f8cf09
Bhuvan Prasad Dahal <bhuvandahal6@gmail.com>
2023-12-27 15:09:12 -0800

    Place details modified

--- client/src/components/Place/PlaceDetails.tsx
+++ client/src/components/Place/PlaceDetails.tsx
@@ -3,12 +3,12 @@ import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 
+import NotFound from '../Utils/NotFound';
 import Loader from '../Utils/Loaders/Loader';
 import ProfileImg from '../../images/profile.png';
 import { State } from '../../interfaces/store';
 import { getPlaceById } from '../../actions/place';
 import { REMOVE_SELECTED_PLACE } from '../../constants/place';
-import NotFound from '../Utils/NotFound';
 
 const PlaceDetails: React.FC = () => {
     const { id } = useParams();
@@ -25,7 +25,7 @@ const PlaceDetails: React.FC = () => {
     const { isLoading, selectedPlace } = useSelector((state: State) => state.place);
 
     if (isLoading) return <Loader />
-    if (!selectedPlace) return <NotFound message={'Place not found!'} />
+    if (!selectedPlace) return <NotFound message='Place not found' />
 
     return (
         <div className='p-3 pb-6 lg:pb-3 bg-dim'>
@@ -43,75 +43,69 @@ const PlaceDetails: React.FC = () => {
                         </Link>
                     </div>
                     <div className='mt-2 flex items-center justify-between'>
-                        <div className='text-dark font-medium'>{selectedPlace?.type?.name}</div>
-                        <div>Share</div>
+                        <div className='text-normal font-medium'>
+                            {[...Array(5)].map((_, index) => (
+                                <i className={`ri-star-fill text-18px ${selectedPlace?.ratings.length >= index ? 'text-secondary' : 'text-grey'}`}></i>
+                            ))}
+                        </div>
+                        <div className='text-dark text-15px'>{moment(selectedPlace?.createdAt).fromNow()}</div>
                     </div>
                     <h1 className='text-20px md:text-22px font-bold text-normal line-clamp-1'>{selectedPlace?.name}</h1>
-                    <button className='underline text-secondarydark hover:text-darkgrey'>See more</button>
+                    <p className='text-dark text-15px mt-n5px pl-1'>{selectedPlace?.type?.name}</p>
                     <div className='my-1 flex items-center gap-2'>
                         <div>
-                            <i className="ri-user-star-line text-26px text-secondary"></i>
+                            <i className="ri-user-star-line text-26px text-secondarydark"></i>
                         </div>
-                        <div className='my-3 flex gap-2'>
-                            <Link to={`/profile/123`}>
-                                <img className='h-30px' src={ProfileImg} alt="profile" />
-                            </Link>
-                            <Link to={`/profile/123`}>
-                                <img className='h-30px' src={ProfileImg} alt="profile" />
-                            </Link>
-                            <Link to={`/profile/123`}>
-                                <img className='h-30px' src={ProfileImg} alt="profile" />
-                            </Link>
-                            <Link to={`/profile/123`}>
-                                <img className='h-30px' src={ProfileImg} alt="profile" />
-                            </Link>
-                        </div>
+                        <Link to={`/profile/${selectedPlace?.owner?.id.toString()}`} className='organizer'>
+                            <img className='h-40px w-40px rounded-full object-cover shadow-box' src={selectedPlace?.owner?.picture || ProfileImg} alt="profile" />
+                            <div className='organizer-name'>{selectedPlace?.owner?.fullName}</div>
+                        </Link>
                     </div>
                     <p className='line-clamp-5'>{selectedPlace?.description}</p>
-                    <button className='underline text-secondarydark hover:text-darkgrey'>Read more</button>
-                    <div className='my-4'>
-                        <h2 className='text-17px font-medium text-dark'>Event Schedules</h2>
-                        <ul>
-                            <li className='flex items-center gap-3 mt-3'>
-                                <div className='h-70px w-70px flex flex-col items-center justify-center border border-solid border-grey rounded-full'>
-                                    <div>9:00</div>
-                                    <div>AM</div>
-                                </div>
-                                <div>
-                                    <h3 className='font-medium'>Opening Ceremony</h3>
-                                    <p className='text-darkgrey'>By Abc Def - 9:00 AM . 10:30 AM</p>
-                                </div>
-                            </li>
-                            <li className='flex items-center gap-3 mt-3'>
-                                <div className='h-70px w-70px flex flex-col items-center justify-center border border-solid border-grey rounded-full'>
-                                    <div>9:00</div>
-                                    <div>AM</div>
-                                </div>
-                                <div>
-                                    <h3 className='font-medium'>Lunch Break</h3>
-                                    <p className='text-darkgrey'>By Abc Def - 9:00 AM . 10:30 AM</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
                 <div className='w-full'>
                     <div className='pb-3 border-b border-solid border-grey'>
-                        <Link to={`/events/12345/book-entry`} className='inline-block py-2 px-5 bg-primary text-white rounded-sm hover:bg-primarydark'>
+                        <Link to={`/events/12345/book-entry`} className='inline-block py-2 px-5 bg-primary text-15px text-white rounded-sm hover:bg-primarydark'>
                             <i className="ri-book-2-line"></i> Book your entry pass
                         </Link>
                     </div>
-                    <div className='py-3 border-b border-solid border-grey'>
-                        <h2>RSVP</h2>
-                        <div>Chart</div>
+                    <div className='py-3 flex justify-between border-b border-solid border-grey'>
+                        <h2><i className="ri-team-line text-18px text-secondarydark"></i> Capacity</h2>
+                        <div>{selectedPlace?.capacity}</div>
                     </div>
                     <div className='py-3 border-b border-solid border-grey'>
-                        <h2>Ticket sales</h2>
-                        <div>Chart</div>
+                        <div className='flex justify-between'>
+                            <h2><i className="ri-facebook-box-line text-18px text-secondarydark"></i> Facebook</h2>
+                            <div>{selectedPlace?.socialMedia?.facebook}</div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <h2><i className="ri-twitter-line text-18px text-secondarydark"></i> Twitter</h2>
+                            <div>{selectedPlace?.socialMedia?.twitter}</div>
+                        </div>
+                        <div className='flex justify-between'>
+                            <h2><i className="ri-mail-line text-18px text-secondarydark"></i> Contact email</h2>
+                            <div>{selectedPlace?.contact}</div>
+                        </div>
+                    </div>
+                    <div className='py-3 flex justify-between border-b border-solid border-grey'>
+                        <h2><i className="ri-price-tag-3-line text-18px text-secondarydark"></i> Price/Hour</h2>
+                        <div>${selectedPlace?.pricePerHour}</div>
                     </div>
                     <div className='py-3 border-b border-solid border-grey'>
-                        <h2>Location</h2>
-                        <div>Map</div>
+                        <h2><i className="ri-cup-line text-18px text-secondarydark"></i> Facilities</h2>
+                        <ul className='pl-2'>
+                            {selectedPlace?.facilities.map((facility) => (
+                                <li>• {facility}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className='py-3 border-b border-solid border-grey'>
+                        <h2><i className="ri-file-list-3-line text-18px text-secondarydark"></i> Terms and Conditions</h2>
+                        <ul className='pl-2'>
+                            {selectedPlace?.termsAndConditions.map((condition) => (
+                                <li>• {condition}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
