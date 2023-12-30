bc5fcb9d0902a7234ff300988d11ce1845c05a91
Bhuvan Prasad Dahal <bhuvandahal6@gmail.com>
2023-12-30 11:06:00 -0800

    Follow component modified

--- client/src/components/Profile/Follow/Card.tsx
+++ client/src/components/Profile/Follow/Card.tsx
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import ProfileImg from '../../../images/profile.png';
+import LoadingImg from '../../../images/loading.gif';
+import { unfollowUser } from '../../../actions/auth';
 
 interface CardProp {
+    tab: string;
+    userId: string;
     id: string;
+    followId: string;
     fullName: string;
     email: string;
     picture: string;
     isLast: boolean;
+    dispatch: any;
 }
 
 const Card: React.FC<CardProp> = ({
+    tab,
+    userId,
     id,
+    followId,
     fullName,
     email,
     picture,
-    isLast
+    isLast,
+    dispatch
 }: CardProp) => {
-    const isMiniLoading = false;
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(true);
+    };
+
+    const isMiniLoading = true;
+
     return (
         <li className={`w-full flex items-center justify-between border border-solid border-grey px-3 py-2 rounded-md ${!isLast ? 'mb-3' : ''}`}>
-            <Link to={`/profile/${id}`} className='inline-flex items-center mt-1 gap-2'>
+            <Link to={`/profile/${followId}`} className='inline-flex items-center mt-1 gap-2'>
                 <img className='h-40px w-40px border rounded-full object-cover shadow-sm' src={picture || ProfileImg} alt="profile" />
                 <div className='text-15px'>
                     <div className='font-medium'>{fullName}</div>
-                    <div className='text-dark'>{email}</div>
+                    <div className='text-dark mt-n5px'>{email}</div>
                 </div>
             </Link>
-            <button className={`relative w-130px py-1 rounded-50px transition-bg duration-300 text-15px ${isMiniLoading ? 'bg-secondary text-dark cursor-not-allowed' : 'bg-primary text-lightgrey hover:bg-primarydark'}`} disabled={isMiniLoading}>
-                <i className="ri-user-unfollow-line"></i> Unfollow
-            </button>
+            {tab === 'following' && (
+                userId === id && (
+                    <button className={`relative w-130px py-1 rounded-50px transition-bg duration-300 text-15px ${isActive && isMiniLoading ? 'bg-secondary text-dark cursor-not-allowed' : 'bg-primary text-lightgrey cursor-pointer hover:bg-primarydark'}`} disabled={isActive && isMiniLoading}>
+                        <img className={`absolute h-35px top-1/2 left-1/2 translate-x-n50p translate-y-n50p ${!isActive || !isMiniLoading ? 'hidden' : ''}`} src={LoadingImg} alt="..." />
+                    </button>
+                )
+            )}
         </li>
     )
 };
--- client/src/components/Profile/Follow/Follow.tsx
+++ client/src/components/Profile/Follow/Follow.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 import Card from './Card';
 import NotFound from '../../Utils/NotFound';
+import Loader from '../../Utils/Loaders/Loader';
 import { getUserById } from '../../../actions/auth';
 import { State } from '../../../interfaces/store';
 import { REMOVE_SELECTED_USER } from '../../../constants/auth';
@@ -11,9 +12,15 @@ import { REMOVE_SELECTED_USER } from '../../../constants/auth';
 const Follow: React.FC = () => {
     const { id } = useParams();
     const location = useLocation();
+    const navigate = useNavigate();
     const dispatch: any = useDispatch();
     const isFollowing = location.pathname.includes('/following');
     const [activeTab, setActiveTab] = useState(isFollowing ? 'following' : 'followers');
+
+    const handleClick = (tab: string) => {
+        setActiveTab(tab);
+        navigate(`/profile/${id}/${tab}`);
+    };
     
     useEffect(() => {
         dispatch(getUserById(id || ''));
@@ -22,16 +29,19 @@ const Follow: React.FC = () => {
         };
     }, []);
 
-    const { isLoading, selectedUser } = useSelector((state: State) => state.auth);
+    const { isLoading, selectedUser, user } = useSelector((state: State) => state.auth);
+
+    if(isLoading) return <Loader />
+    if(!selectedUser) return <NotFound message='User not found' />
 
     return (
         <div className='bg-dim min-h-full flex justify-center p-3'>
             <div className='bg-white max-w-xl w-full p-4 rounded-lg shadow-box'>
                 <div className='flex gap-5 md:gap-10'>
-                    <div onClick={() => setActiveTab('following')} className={`w-110px py-1 border-b-3 border-solid flex items-center justify-center gap-1 cursor-pointer ${activeTab === 'following' ? 'border-secondary text-primarydark font-medium' : ''}`}>
+                    <div onClick={() => handleClick('following')} className={`w-110px py-1 border-b-3 border-solid flex items-center justify-center gap-1 cursor-pointer ${activeTab === 'following' ? 'border-secondary text-primarydark font-medium' : ''}`}>
                         <i className={`${activeTab === 'following' ? 'ri-user-heart-fill' : 'ri-user-heart-line'} text-22px`}></i> Following
                     </div>
-                    <div onClick={() => setActiveTab('followers')} className={`w-110px py-1 border-b-3 border-solid flex items-center justify-center gap-1 cursor-pointer ${activeTab === 'followers' ? 'border-secondary text-primarydark font-medium' : ''}`}>
+                    <div onClick={() => handleClick('followers')} className={`w-110px py-1 border-b-3 border-solid flex items-center justify-center gap-1 cursor-pointer ${activeTab === 'followers' ? 'border-secondary text-primarydark font-medium' : ''}`}>
                         <i className={`${activeTab === 'followers' ? 'ri-user-star-fill' : 'ri-user-star-line'} text-22px`}></i> Followers
                     </div>
                 </div>
@@ -41,11 +51,15 @@ const Follow: React.FC = () => {
                             [...selectedUser?.following].reverse().map((followed, index) => (
                                 <Card
                                     key={index}
-                                    id={followed?.id?.toString()}
+                                    tab='following'
+                                    userId={user?._id?.toString()}
+                                    id={id || ''}
+                                    followId={followed?.id?.toString()}
                                     fullName={followed?.fullName}
                                     email={followed?.email}
                                     picture={followed?.picture}
                                     isLast={index === selectedUser?.following?.length - 1}
+                                    dispatch={dispatch}
                                 />
                             ))
                         ) : (
@@ -56,11 +70,15 @@ const Follow: React.FC = () => {
                             [...selectedUser?.followers].reverse().map((follower, index) => (
                                 <Card
                                     key={index}
-                                    id={follower?.id?.toString()}
+                                    tab='followers'
+                                    userId={user?._id?.toString()}
+                                    id={id || ''}
+                                    followId={follower?.id?.toString()}
                                     fullName={follower?.fullName}
                                     email={follower?.email}
                                     picture={follower?.picture}
                                     isLast={index === selectedUser?.followers?.length - 1}
+                                    dispatch={dispatch}
                                 />
                             ))
                         ) : (
